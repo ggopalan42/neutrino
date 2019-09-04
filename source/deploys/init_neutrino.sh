5326111aa7d48cc7d14839c32f2f179a72cb2c9d
Giri Gopalan <ggopalan@yahoo.com>
2019-09-03 17:40:37 -0700

    Fixed typos

--- source/deploys/init_neutrino.sh
+++ source/deploys/init_neutrino.sh
@@ -4,7 +4,7 @@ This is fake (pseudo) code for initializing neutrino app.
 This is supposed to be run once per deploy
 
 1. Run docker/docker_make_base_images.sh
-    - This will make the base python and opencv images for furtehr use
+    - This will make the base python and opencv images for further use
 2. Run "make build" on docker/cam2pub dir
     - This will make the main cam2pub image for use by compose
 3. Run: ./cloud/aws/aws_iot_core/init_iot_core.py. 

f61fed81924ada3d67460f392da59cf0a445b310
Giri Gopalan <ggopalan@yahoo.com>
2019-09-19 18:25:52 -0700

    Added the SafeLoader option since just plain load is deprecated

--- source/utils/file_utils.py
+++ source/utils/file_utils.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-def yaml2dict(yaml_fn):
+def yaml2dict(yaml_fn, Loader=yaml.SafeLoader):
     ''' Read from yaml_fn and return a dict '''
     with open(yaml_fn) as fh:
         yaml_dict = yaml.load(fh)
